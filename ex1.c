a044703793d5409e96748da755242c5d829a173c
Nigeriaboy <abdurahmonabdullah247@gmail.com>
2024-02-22 22:21:03 +0100

    C exercise

---
+++ ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int sum = 50 + 25;
+
+	printf("Programming is fun.\nAnd programming in C is even more fun.\n");
+
+	printf("\nTest...\n..1\n...2\n....3\n");
+
+	printf("The sum of 50 and 25 is %d\n", sum);
+
+	return(0);
+}
---
+++ ndm.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Name    : ABDULLAH ABDURAHMON.\nDOB     : February 20, 2006.\nMobile  : +234-701-6712-983.\n");
+
+	return (0);
+}
